ce38e84981e69e4d5091f66fd9ccb2a71a9b4c1e
Rony <frrony1996@gmail.com>
2022-05-01 01:48:17 +0500

    delete api added

--- index.js
+++ index.js
@@ -21,18 +21,22 @@ async function run() {
     try {
         await client.connect()
         const bikesCollection = client.db("Bike-server").collection("bikes");
+        const addedCollection = client.db("Bike-server").collection("addedCollection")
+        // get all data 
         app.get("/bikes", async (req, res) => {
             const query = {}
             const cursor = bikesCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
         })
+        // get a single data 
         app.get('/bikes/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectID(id) }
             const result = await bikesCollection.findOne(query)
             res.send(result)
         })
+        // update existing data 
         app.put('/bikes/:id', async (req, res) => {
             const id = req.params.id;
             console.log(req)
@@ -45,6 +49,44 @@ async function run() {
                 }
             }
             const result = await bikesCollection.updateOne(query, updatedDoc, options)
+            res.send(result)
+        })
+
+
+        // create addeded collection 
+        app.post('/bikes', async (req, res) => {
+            const additem = req.body;
+            const doc = {
+                name: additem.name,
+                email: additem.email,
+                quantity: additem.quantity,
+                picture: additem.photourl,
+            }
+            const result = await addedCollection.insertOne(doc)
+            res.send(result)
+        })
+        // get added Collection 
+        app.get('/addedCollection', async (req, res) => {
+            const email = req.query.email;
+            console.log(email)
+            const query = { email: email }
+            const cursor = addedCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        // delete item added collection 
+        app.delete('/addedCollection/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) }
+            const result = await addedCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // delete the existing item 
+        app.delete('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) }
+            const result = await bikesCollection.deleteOne(query)
             res.send(result)
         })
 
